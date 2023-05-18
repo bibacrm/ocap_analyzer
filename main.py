33967e2015047bb93be2fe061d1b374c4012a443
bibacrm <79942827+bibacrm@users.noreply.github.com>
2023-05-19 00:10:53 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import requests
 import logging
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 from config import *
 from processing.process_attendance import get_attendance
@@ -30,6 +30,7 @@ def index():
     tag = request.args.get('mission_tag')
     filename = request.args.get('filename')
     data_url = f'{REPLAY_FILE_URL}{filename}' if filename is not None else None
+    data_only = request.args.get('data_only')
     # Getting submitted form parameter with a direct OCAP replay json file URL
     if request.method == 'POST':
         data_url = request.form['ocap_url']
@@ -63,6 +64,26 @@ def index():
     elif stats_report['tag'] in MISSION_TAGS_FOR_TOTAL_STATS_VTN:
         tag = VTN_MODE_ID
 
+    if data_only is not None:
+        if filename is not None:
+            response_body = {
+                'stat_data': stats_report['frag_stats'],
+                'team_stat_data': stats_report['team_stats'],
+                'ko_stats_data': stats_report['ko_stats'],
+                'connected_stats_data': stats_report['connected_stats'],
+                'mission_name': stats_report['mission_name'],
+                'mission_author': stats_report['mission_author'],
+                'mission_duration': stats_report['mission_duration'],
+                'sides': stats_report['sides'],
+                'winner': stats_report['winner'],
+                'filename': filename
+            }
+        else:
+            response_body = {
+                'replay_list': replay_list_data
+            }
+        return jsonify(response_body), 200
+
     return render_template(
         'index2.html',
         tag=tag,
@@ -85,6 +106,20 @@ def index():
 @app.route('/total_<tag>', methods=['GET'])
 def total(tag):
     stats_report = process_total_stats(tag, calculate=False)
+    data_only = request.args.get('data_only')
+
+    if data_only is not None:
+        response_body = {
+            'cache_count': stats_report['cache_count'],
+            'tk_stats': stats_report['tk_stats'],
+            'tk_stats_steam': stats_report['tk_stats_steam'],
+            'team_tk_stats': stats_report['team_tk_stats'],
+            'frag_stats': stats_report['frag_stats'],
+            'frag_stats_steam': stats_report['frag_stats_steam'],
+            'team_frag_stats': stats_report['team_frag_stats'],
+            'ks_win_stat': stats_report['ks_win_stat']
+        }
+        return jsonify(response_body), 200
 
     return render_template(
         'index_total.html',
@@ -106,8 +141,20 @@ def total(tag):
 def player_total(tag):
     player_name = request.args.get('player_name')
     steam_id = request.args.get('steam_id')
+    data_only = request.args.get('data_only')
 
     stats_report = get_player_stats(player_name, tag, steam_id)
+
+    if data_only is not None:
+        response_body = {
+            'player_name': player_name,
+            'steam_id': steam_id if steam_id is not None else stats_report['steam_id'],
+            'tk_stats': stats_report['tk_stats'],
+            'frag_stats': stats_report['frag_stats'],
+            'ks_win_stat': stats_report['ks_win_stat'],
+            'ks_missions': stats_report['ks_missions']
+        }
+        return jsonify(response_body), 200
 
     return render_template(
         'index_player.html',
@@ -126,6 +173,7 @@ def player_total(tag):
 @app.route('/attendance', methods=['GET'])
 def attendance():
     tag = request.args.get('tag')
+    data_only = request.args.get('data_only')
 
     stats_report = get_attendance(tag)
 
@@ -135,12 +183,25 @@ def attendance():
     rotation_2_1 = sorted(TVT_2_ROTATION_TEAMS['1'])
     rotation_2_2 = sorted(TVT_2_ROTATION_TEAMS['2'])
 
-    if tag == 'if':
-        rotation_1_1 = sorted(IF_ROTATION_TEAMS['1'])
-        rotation_1_2 = sorted(IF_ROTATION_TEAMS['2'])
-    elif tag == 'vtn':
-        rotation_1_1 = sorted(VTN_ROTATION_TEAMS['1'])
-        rotation_1_2 = sorted(VTN_ROTATION_TEAMS['2'])
+    if tag != 'tvt':
+        rotation_2_1 = []
+        rotation_2_2 = []
+        if tag == 'if':
+            rotation_1_1 = sorted(IF_ROTATION_TEAMS['1'])
+            rotation_1_2 = sorted(IF_ROTATION_TEAMS['2'])
+        elif tag == 'vtn':
+            rotation_1_1 = sorted(VTN_ROTATION_TEAMS['1'])
+            rotation_1_2 = sorted(VTN_ROTATION_TEAMS['2'])
+
+    if data_only is not None:
+        response_body = {
+            'attendance': stats_report['attendance'],
+            'rotation_1_1': rotation_1_1,
+            'rotation_1_2': rotation_1_2,
+            'rotation_2_1': rotation_2_1,
+            'rotation_2_2': rotation_2_2
+        }
+        return jsonify(response_body), 200
 
     return render_template(
         'index_attendance2.html',
